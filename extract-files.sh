d964b85122957230fa344ed645659b033931944d
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2017-12-10 17:13:43 -0500

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=essential
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-TESLA_ROOT="$MY_DIR"/../../..
+CM_ROOT="$MY_DIR"/../../..
 
-HELPER="$TESLA_ROOT"/vendor/tesla/build/tools/extract_utils.sh
+HELPER="$CM_ROOT"/vendor/tesla/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +51,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$TESLA_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
