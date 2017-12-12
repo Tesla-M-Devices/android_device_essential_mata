05be5485e37e9650712dee3e140bc4b6d1aa4880
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2017-12-12 09:02:11 -0500

    Remove payload

--- device.mk
+++ device.mk
@@ -257,9 +257,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
-# Update engine
-PRODUCT_PACKAGES += brillo_update_payload
-
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
